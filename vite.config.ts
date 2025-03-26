dc7f2b719bf606d90a053e675344ec739db4d5b5
杨静波 <alex@defipay.biz>
2025-03-26 14:48:11 +0800

    feat:测试

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "development" ? "/" : "./",
   plugins: [vue()],
   resolve: {
     alias: {
